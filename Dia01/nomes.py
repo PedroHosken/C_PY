2a5e91edaf498d62a02647f2deffb248049e01dc
Pedro Hosken Fernandes Guimaraes <Pg87066@gmail.com>
2024-12-01 17:59:33 -0300

    Fim de D1

--- Dia01/nomes.py
+++ Dia01/nomes.py
@@ -6,3 +6,16 @@ print("Bill Gates")
 
 
 # %%
+print("Ritinha meu amor")
+# %%
+#Isso é um comentário
+# %%
+print(10/2)
+# %%
+#type é para tipo de objeto
+type(5.0)
+# %%
+print(7%3)
+# %%
+print(7//3)
+# %%
