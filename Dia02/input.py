19ed6d6e564dac3c6db99d5d34a8314860bd8ab8
Pedro Hosken Fernandes Guimaraes <Pg87066@gmail.com>
2025-01-10 19:06:35 -0300

    Aprendemos váriaveis

---
+++ Dia02/input.py
@@ -0,0 +1,1 @@
+#Aprendendo a função input
---
+++ Dia02/var.py
@@ -0,0 +1,19 @@
+# Celúla de execução # %%
+# %%
+print("Voltamos  10/01/2025")
+# Vamos atribuir o texto à uma váriavel
+# %%
+nome = "Luluzinha"
+print(nome)
+print("Hoje eu vi a", nome)
+# nova atribuição ao nome
+nome = "Márcia"
+print("Hoje eu vi a", nome)
+# %%
+# Agora númericos
+
+a = 10
+b = 15
+
+print(a + b)
+# %%
