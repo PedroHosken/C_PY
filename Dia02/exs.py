1cdd1ef917ba35ca24654f724e877c1fc5a42c43
Pedro Hosken Fernandes Guimaraes <Pg87066@gmail.com>
2025-01-10 19:52:56 -0300

    Exercícios finalizados e corrigidos

---
+++ Dia02/exs.py
@@ -0,0 +1,36 @@
+# %%
+# Maratona de Exercícios
+# Ex01 = Bom dia
+
+
+
+frase01 = "Bom"
+frase02 = "dia"
+
+print(frase01+ " " +frase02)
+# %%
+# Bom dia Completo
+frase01 = "Bom dia"
+print(frase01)
+nome = input("Qual é seu nome ?\n")
+print("Prazer te conhecer", nome)
+
+
+# %%
+# História interativa
+p01 = input("Havia um menino que morava perto do Rio, ele não tinha o que comer, assi... APERTE ENTER")
+p02 = input("Ele precisou começar a trabalhar, começou a pescar todos os dias da semana.")
+
+# %%
+import math # Tem que estar dentro da célula
+# Raiz Quadrada de um número
+a = int(input("Coloque o valor: "))
+a = math.sqrt(a)
+print(a)
+# ou utilizar a = a ** 0.5
+# %%
+# Dobro do número inserido pelo Usuário
+a = int(input("Insira e receba o dobro :)"))
+print(2*a)
+
+# %%
