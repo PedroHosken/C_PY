e58921c56900b83a41561c89d0d2b69693b97576
Pedro Hosken Fernandes Guimaraes <Pg87066@gmail.com>
2025-01-10 19:23:36 -0300

    Aprendemos input

--- Dia02/input.py
+++ Dia02/input.py
@@ -1,1 +1,27 @@
-#Aprendendo a função input
+# %% 
+# Aprendendo a função input = ler do teclado
+nome = input("Entre com o seu nome : ")
+print("Seu nome é :", nome)
+# %%
+
+a = input("Entre com o valor de a")
+b = input("Entre com o valor de b")
+
+soma = a + b
+print(soma)
+# equivale
+print(a + b) # concatenação
+
+
+# %%
+# conversão para inteiro
+a = input("Entre com o valor de a")
+a = int(a)
+b = input("Entre com o valor de b")
+b = int(b)
+
+soma = a + b
+print(soma)
+# %%
+# Exemplo de conversão pelo input
+a = int(input("Entre com o valor de a"))
